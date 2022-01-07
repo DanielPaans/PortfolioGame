8d8ae38f838bdbd4ac29920f99b648daafa26760
DanielPaans <daniel100tablets@gmail.com>
2022-01-07 12:17:56 +0100

    updated all dependencies

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
-
 import Phaser from "phaser";
 
-const config = {
+const MAP_WIDTH = 1600;
+
+const WIDTH = document.body.offsetWidth;
+const HEIGHT = 600;
+const ZOOM_FACTOR = 1.5;
+
+const SHARED_CONFIG = {
+  mapOffset: {x: MAP_WIDTH > WIDTH ? MAP_WIDTH - WIDTH : 0, y: 200},
+  width: WIDTH,
+  height: HEIGHT,
+  zoomFactor: ZOOM_FACTOR,
+  debug: true,
+  leftTopCorner: {
+    x: (WIDTH - (WIDTH/ZOOM_FACTOR)) / 2,
+    y: (HEIGHT - (HEIGHT/ZOOM_FACTOR)) / 2
+  },
+  rightTopCorner: {
+    x: ((WIDTH / ZOOM_FACTOR) + ((WIDTH - (WIDTH / ZOOM_FACTOR)) / 2)),
+    y: (HEIGHT - (HEIGHT/ZOOM_FACTOR)) / 2
+  },
+  rightBottomCorner: {
+    x: ((WIDTH / ZOOM_FACTOR) + ((WIDTH - (WIDTH / ZOOM_FACTOR)) / 2)),
+    y: ((HEIGHT / ZOOM_FACTOR) + ((HEIGHT - (HEIGHT / ZOOM_FACTOR)) / 2))
+  },
+  lastLevel: 2
+}
+
+const SCENES = [PreloadScene];
+const initScenes = () => SCENES.map(Scene => new Scene(SHARED_CONFIG));
+
+const CONFIG = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
+  pixelArt: true,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      debug: SHARED_CONFIG.debug
     }
   },
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
-
-new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
+  scene: initScenes()
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
-}
+new Phaser.Game(CONFIG);
---
+++ src/scenes/Preload.js
@@ -0,0 +1,8 @@
+
+class PreloadScene extends Phaser.Scene {
+
+    constructor(config) {
+        super('PreloadScene');
+        this.config = config;
+    }
+}
